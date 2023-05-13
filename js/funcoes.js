460e0bc0bf2964b0335747222ba57d31fef75196
Renan Aragao <renan.aragao159@gmail.com>
2023-05-13 01:10:54 -0300

    Finalizando a dashboard junto com o preloader

--- js/funcoes.js
+++ js/funcoes.js
@@ -13,6 +13,11 @@ $(document).ready(function(){
     // Chamada para ativar o sidenav lateral
     $('.sidenav').sidenav();
 
+    // Chamada para o botao flutuante
+    $('.fixed-action-btn').floatingActionButton({
+      hoverEnabled: false
+    });
+
 });
 
 
@@ -24,3 +29,9 @@ function fullScreen() {
       document.exitFullscreen();
   }
 }
+
+  /* Preloader */
+  $(window).load(function () {
+  $(".loader").delay(1500).fadeOut("slow"); //retire o delay quando for copiar!
+  $("#tudo_page").toggle("fast");
+});
